b3f3e7a3df8d794e4d816c7e781cc1975a5eda77
Ralf Hagemeister <ralf.hagemeister@lernsteine.de>
2025-07-09 11:16:29 +0200

    corrected | last changes

--- block_masterdashboard.php
+++ block_masterdashboard.php
@@ -1,43 +1,11 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Masterdashboard block definition.
- *
- * @package   block_masterdashboard
- * @copyright 2025 Ralf Hagemeister <ralf.hagemeister@lernsteine.de>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-/**
- * Class block_masterdashboard - provides the main dashboard block.
- */
+// ...
 class block_masterdashboard extends block_base {
-    /**
-     * Initializes the block with a title.
-     */
     public function init() {
         $this->title = '';
     }
 
-    /**
-     * Generates the block content.
-     *
-     * @return stdClass
-     */
     public function get_content() {
         global $USER, $CFG, $OUTPUT;
 
@@ -51,7 +19,6 @@ class block_masterdashboard extends block_base {
         $this->page->requires->css(new moodle_url('/blocks/masterdashboard/styles.css'));
 
         $courses = enrol_get_users_courses($USER->id, true, '*');
-
         uasort($courses, function ($a, $b) {
             return ($b->enddate ?? 0) <=> ($a->enddate ?? 0);
         });
@@ -103,8 +70,17 @@ class block_masterdashboard extends block_base {
             $status = '';
             if ($iscomplete) {
                 $status = 'completed';
-                $dateinfo = get_string('completedon', 'block_masterdashboard') . ': ' .
-                    userdate(time(), '%A, %d.%m.%Y');
+
+                $completedon = '';
+                $completions = $completioninfo->get_completions($USER->id);
+                foreach ($completions as $completion) {
+                    if ($completion->is_complete() && !empty($completion->timecompleted)) {
+                        $completedon = userdate($completion->timecompleted, '%A, %d.%m.%Y');
+                        break;
+                    }
+                }
+
+                $dateinfo = get_string('completedon', 'block_masterdashboard') . ': ' . $completedon;
             } else if (!empty($course->enddate) && time() > $course->enddate) {
                 $status = 'overdue';
                 $dateinfo = get_string('duedate', 'block_masterdashboard') . ': ' .
